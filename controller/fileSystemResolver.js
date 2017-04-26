8b21ca8f3fbe86eea0a3bc88f5c248f75e6c0bda
KaushikIyer16 <kaushiiyer@gmail.com>
2017-04-26 10:04:19 +0530

    added file opening

--- controller/fileSystemResolver.js
+++ controller/fileSystemResolver.js
@@ -34,6 +34,10 @@ export class FileSystemResolver{
     );
   }
 
+  containsTilde(argument){
+    return(argument[0] === '~' ? true : false);
+  }
+
   isDirectory(argument){
     if(argument[1] === 'd' && argument[0] === '-'){
       return true;
@@ -136,6 +140,7 @@ export class FileSystemResolver{
     let argv = this.parseString.trim().split(' ');
     let directoryList = new Array();
     let fileList = new Array();
+    let openFileList = new Array();
     let currentDirectory = "",
         currentFormat    = "",
         rootPath         = this.getRoothPathFromModal();
@@ -182,7 +187,14 @@ export class FileSystemResolver{
           // print("---"+rootPath+currentDirectory+argv[i]+currentFormat+"---\n");
           try {
             // createFile(rootPath+currentDirectory+argv[i]+currentFormat);
-            fileList.push(rootPath+currentDirectory+argv[i]+currentFormat);
+            if (this.containsTilde(argv[i])) {
+              openFileList.push(rootPath+currentDirectory+argv[i].substring(1)+currentFormat);
+              fileList.push(rootPath+currentDirectory+argv[i].substring(1)+currentFormat);
+            }
+            else{
+              fileList.push(rootPath+currentDirectory+argv[i]+currentFormat);
+            }
+
           } catch (e) {
             print("error creating the file");
           }
@@ -191,11 +203,16 @@ export class FileSystemResolver{
 
       }
       let allValidFiles = true; // change this to false when you finish the package
+
       for (let i = 0; i < directoryList.length; i++) {
         createDirectory(directoryList[i]);
       }
       for (let i = 0; i < fileList.length; i++) {
         createFile(fileList[i]);
+      }
+
+      for (var i = 0; i < openFileList.length; i++) {
+        atom.workspace.open(openFileList[i]);
       }
       if (allValidFiles) {
         atom.notifications.addSuccess("\nFolders/Files Successfully Created\n");
@@ -206,6 +223,7 @@ export class FileSystemResolver{
     } else {
       // print("\nEMPTY FILE STRUCTURE GIVEN TO FSB\n");
       // here i will add a notification to say that an empty structure is given
+
       atom.notifications.addWarning("\nEMPTY FILE STRUCTURE GIVEN TO FSB\n");
     }
 
@@ -215,7 +233,7 @@ export class FileSystemResolver{
     let argv = this.parseString.trim().split(' ');
     let baseMargin = 'calc(2% + ';
     let addedMargin = 0;
-    clearList('.list-group');
+    clearList('.fsb-list-group');
 
     let currentDirectory = "",
         currentFormat    = "";
@@ -251,24 +269,35 @@ export class FileSystemResolver{
           continue;
         }else if(currentFormat === "" && !this.containsFormat(argv[i]) ){
 
-
-          addChildToParent('.list-group',li(true,argv[i],computeMargin(baseMargin,addedMargin)));
+          if (this.containsTilde(argv[i])) {
+            addChildToParent('.fsb-list-group',li(true,argv[i].substring(1),computeMargin(baseMargin,addedMargin)));
+          }
+          else{
+            addChildToParent('.fsb-list-group',li(true,argv[i],computeMargin(baseMargin,addedMargin)));
+          }
           // print("--"+currentDirectory+"\n");
         }else{
           // here i need to add nodes to the tree and create a complete tree
-          addChildToParent('.list-group',li(false,argv[i]+currentFormat,computeMargin(baseMargin,addedMargin)));
+          if (this.containsTilde(argv[i])) {
+            addChildToParent('.fsb-list-group',li(false,(argv[i]+currentFormat).substring(1),computeMargin(baseMargin,addedMargin)));
+          }
+          else{
+            addChildToParent('.fsb-list-group',li(false,argv[i]+currentFormat,computeMargin(baseMargin,addedMargin)));
+          }
+
           // print("---"+currentDirectory+argv[i]+currentFormat+"---\n");
 
         }
 
       }
-      addChildToParent('.list-group',li(false,'...',computeMargin(baseMargin,0)));
+      addChildToParent('.fsb-list-group',li(false,'...',computeMargin(baseMargin,0)));
 
     } else {
       // print("\nEMPTY FILE STRUCTURE GIVEN TO FSB\n");
       // here i will add a notification to say that an empty structure is given
-      clearList('.list-group');
-      addChildToParent('.list-group',li(false,'...',computeMargin(baseMargin,0)));
+      clearList('.fsb-list-group');
+
+      addChildToParent('.fsb-list-group',li(false,'...',computeMargin(baseMargin,0)));
     }
   }
 
